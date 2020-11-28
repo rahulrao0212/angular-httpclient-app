9d9d6953d06ce624b4bfea4e4be2e0fdeb407a2c
Rahul Rao <RAOR378647@exactsoftware.com>
2020-11-28 20:34:45 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Repos } from './Model/repo.model';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'htppclient-app';
+export class AppComponent implements OnInit {
+
+  userName: string = "tektutorialshub"
+  baseURL: string = "https://api.github.com/";
+  repos: Repos[] = [];
+
+
+  constructor(private http: HttpClient) {
+  }
+
+  ngOnInit() {
+    this.getRepos()
+  }
+
+
+  public getRepos() {
+
+    return this.http.get<Repos[]>(this.baseURL + 'users/' + this.userName + '/repos')
+      .subscribe(
+        (response) => {                           //Next callback
+          console.log('response received')
+          console.log(response);
+          this.repos = response;
+        },
+        (error) => {                              //Error callback
+          console.error('Request failed with error')
+          alert(error);
+        },
+        () => {                                   //Complete callback
+          console.log('Request completed')
+        })
+  }
 }
